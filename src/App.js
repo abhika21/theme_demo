b2238925bac3473309c6f2166b254693cea6de13
abhishek <avinashk8430@gmail.com>
2024-10-28 15:16:51 +0530

    add my name

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ function App() {
       backgroundColor: "#FFD700",
       text: "Kiran Theme",
     },
-    Abhishekk: {
+    Abhi: {
       backgroundColor: "#87CEEB",
-      text: "Abhishek Theme.",
+      text: "Abhi Theme.",
     },
   };
 
@@ -31,7 +31,7 @@ function App() {
         <label className="namebox1" htmlFor="theme">Theme:</label>
         <select className="namebox" id="branch" onChange={handleThemeChange}>
           <option value="Kiran" className="b1">Kiran</option>
-          <option value="Abhishekk" className="b1">Abhishek</option>
+          <option value="Abhi" className="b1">Abhi</option>
         </select>
       </div>
 
