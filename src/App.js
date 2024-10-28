dc02c9f25cfeb57ada3922324b3ede3e51e295cd
kiran783 <kirannaik0406@gmail.com>
2024-10-28 14:11:01 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [theme, setTheme] = useState("Kiran");
+
+  const handleThemeChange = (event) => {
+    setTheme(event.target.value);
+  };
+
+  const themeStyles = {
+    Select: {
+      backgroundColor: "#FFL700",
+      text: "Select One",
+    },
+    Kiran: {
+      backgroundColor: "#FFD700",
+      text: "Kiran Theme",
+    },
+    Abhishek: {
+      backgroundColor: "#87CEEB",
+      text: "Abhishek Theme.",
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Theme Change</h1>
+
+      <div className="branchbox">
+        <label className="namebox1" htmlFor="theme">Theme:</label>
+        <select className="namebox" id="branch" onChange={handleThemeChange}>
+          <option value="Kiran" className="b1">Kiran</option>
+          <option value="Abhishek" className="b1">Abhishek</option>
+        </select>
+      </div>
+
+      <div
+        style={{
+          padding: "20px",
+          color: "#333",
+          backgroundColor: themeStyles[theme]?.backgroundColor || "#FFF",
+          borderRadius: "10px",
+        }}
+      >
+        <p style={{ fontSize: "18px" }}>{themeStyles[theme]?.text || "Select a theme"}</p>
+      </div>
     </div>
   );
 }
